2286ef9015782e0b6ad25c6bdb4abcea1e445b75
John-Henry Liberty <johnhenry.liberty@gmail.com>
2016-05-09 15:53:07 -0500

    Use import in session unit test

--- test/unit/packages/session.js
+++ test/unit/packages/session.js
@@ -3,7 +3,7 @@ import { randomBytes } from 'crypto';
 import fetch from 'isomorphic-fetch';
 
 import encrypt from '../../../src/packages/session/utils/encrypt';
-var { default: config } = require('../../test-app/config/environments/test');
+import config from '../../test-app/config/environments/test';
 
 const host = 'http://localhost:4000';
 
