fdfe19811daf2361913442cc8864252b43335ee7
Kevin Ngao <kevgong@yahoo.com>
2016-07-11 16:41:13 -0400

    feat: check directory structure is valid before starting project

--- src/packages/cli/commands/build.js
+++ src/packages/cli/commands/build.js
@@ -3,6 +3,10 @@ import Ora from 'ora';
 import { CWD, NODE_ENV } from '../../../constants';
 import { compile } from '../../compiler';
 
+import fs from '../../fs';
+import { red } from 'chalk';
+import path from 'path';
+
 export async function build(useStrict: boolean = false): Promise<void> {
   const spinner = new Ora({
     text: 'Building your application...',
@@ -11,9 +15,24 @@ export async function build(useStrict: boolean = false): Promise<void> {
 
   spinner.start();
 
-  await compile(CWD, NODE_ENV, {
-    useStrict
+  await Promise.all([
+    fs.readdirAsync(path.join(CWD, 'app', 'models')),
+    fs.readdirAsync(path.join(CWD, 'app', 'controllers')),
+    fs.readdirAsync(path.join(CWD, 'app', 'serializers')),
+    fs.readdirAsync(path.join(CWD, 'db', 'migrate')),
+    fs.readdirAsync(path.join(CWD, 'config', 'environments')),
+  ]).catch(err => {
+    spinner.stop();
+    console.log(red('Project directory structure is invalid'));
+    console.log(red('You must be in a root directory of a project to build'));
+    console.log(red(err));
+    process.exit(1);
   });
 
+  await compile(CWD, NODE_ENV, {
+      useStrict
+    });
+  
   spinner.stop();
+
 }
