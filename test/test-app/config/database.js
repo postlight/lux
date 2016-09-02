b3a0e07d9352aa78f61401274f938371203d1137
Zachary Golba <zachary.golba@postlight.com>
2016-09-01 21:05:56 -0400

    fix: use config object for database

--- test/test-app/config/database.js
+++ test/test-app/config/database.js
@@ -1,10 +1,23 @@
-const { env: { DATABASE_DRIVER = 'sqlite3' } } = process;
-const environments = ['development', 'test', 'production'];
+const {
+  env: {
+    DATABASE_DRIVER,
+    DATABASE_USERNAME,
+    DATABASE_PASSWORD,
+  }
+} = process;
 
-export default environments.reduce((config, key) => ({
-  ...config,
-  [key]: {
+export default {
+  development: {
+    driver: 'sqlite3',
+    database: 'lux_test'
+  },
+  test: {
     driver: DATABASE_DRIVER,
+    username: DATABASE_USERNAME,
+    database: DATABASE_PASSWORD
+  },
+  production: {
+    driver: 'sqlite3',
     database: 'lux_test'
   }
-}), {});
+};
