731475a79007bc1c6bc236f42d01fc653533f1f9
Nick Schot <nickschot@gmail.com>
2017-01-21 20:36:47 +0100

    Remove eslint line disable

--- src/packages/database/query/index.js
+++ src/packages/database/query/index.js
@@ -165,7 +165,6 @@ class Query<+T: any> extends Promise {
         this.snapshots = this.snapshots
           .filter(([method]) => method !== 'orderByRaw')
           .concat([
-            // eslint-disable-next-line prefer-template
             ['orderByRaw', uniq([columnName, this.model.primaryKey])
               .map(key => `${this.model.tableName}.${key} ${direction}`)
               .join(', ')
