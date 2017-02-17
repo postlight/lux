47f6679d7052489d3f3085cdda7c0debc4706cfc
Zachary Golba <zachary.golba@postlight.com>
2017-02-17 11:05:14 -0500

    fix: properly set database driver in circleci

--- test/test-app/config/database.js
+++ test/test-app/config/database.js
@@ -1,12 +1,41 @@
+const {
+  env: {
+    DATABASE_DRIVER,
+    DATABASE_USERNAME,
+    DATABASE_PASSWORD,
+    CIRCLE_NODE_INDEX,
+  }
+} = process;
+
+let driver;
+
+switch (CIRCLE_NODE_INDEX) {
+  case '0':
+    driver = 'pg';
+    break;
+
+  case '1':
+    driver = 'mysql2';
+    break;
+
+  case '2':
+    driver = 'sqlite3';
+    break;
+
+  default:
+    driver = DATABASE_DRIVER || 'sqlite3';
+    break;
+}
+
 export default (
   ['development', 'test', 'production'].reduce((config, env) => (
     Object.assign(config, {
       [env]: {
+        driver,
         pool: 2,
-        driver: process.env.DATABASE_DRIVER || 'sqlite3',
         database: 'lux_test',
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
+        username: DATABASE_USERNAME,
+        password: DATABASE_PASSWORD,
       },
     })
   ), {})
